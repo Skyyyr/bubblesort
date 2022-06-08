bd2c1376248b5fb74edcd7e73f0b3a9f9af231ef
Skyyyr <emuswgluvr@gmail.com>
2022-06-08 14:49:18 -0700

    Finished bubble sort

--- js/bubbleSort.js
+++ js/bubbleSort.js
@@ -3,5 +3,18 @@ var swaps = 0
 
 // Your Code Here
 
-console.log("Final result: " + result)
+function bubbleSort(array) {
+    const arr = array.slice();
+    for (let i = 0; i < arr.length-1; i++) {
+        for (let j = 0; j < arr.length-1-i; j++) {
+            if (arr[j] > arr[j+1]) {
+                [arr[j], arr[j+1]] = [arr[j+1], arr[j]]
+                swaps++;
+            }
+        }
+    }
+    return arr
+}
+
+console.log("Final result: " + bubbleSort(sequence))
 console.log("Swaps: " + swaps)
--- python/bubble_sort.py
+++ python/bubble_sort.py
@@ -1,7 +1,27 @@
 sequence = [4, 3, 5, 0, 1]
-swaps = 0
+
+
+# swaps = 0
+
 
 # Your Code Here
 
-print(f"Final result: {result}")
-print(f"Swaps: {swaps}")
+def bubble(list_a):
+    indexing_length = len(list_a) - 1
+    _sorted = False
+    swaps = 0
+    while not _sorted:
+        _sorted = True
+
+        for i in range(0, indexing_length):
+            if list_a[i] > list_a[i + 1]:
+                _sorted = False
+                list_a[i], list_a[i + 1] = list_a[i + 1], list_a[i]
+                swaps += 1
+    return list_a, swaps
+
+
+print(bubble([4, 8, 1, 14, 8, 2, 9, 5, 7, 6, 6]))
+print(bubble(sequence))
+# print(f"Final result: {sequence}")
+# print(f"Swaps: {swaps}")
